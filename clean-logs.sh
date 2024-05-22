fd6bd7992f454314a68b6e141aa5215df82fa172
Tyelr A Stank <tylerastank@gmail.com>
2024-05-22 16:55:14 -0500

    Removed vestigial comment

--- clean-logs.sh
+++ clean-logs.sh
@@ -4,7 +4,6 @@ TIME=`date +%H%M%S`
 OLD_LOGDIR="/home/pi/old_logs/$DATE/$TIME"
 mkdir -p $OLD_LOGDIR
 mv ~/log/* $OLD_LOGDIR
-# TODO make the telemetry script create these directories if necessary
 mkdir -p /home/pi/log/read_sensors
 mkdir -p /home/pi/log/telemetry
 mkdir -p /home/pi/log/racebox
