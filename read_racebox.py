d8d9a0f0d7ea18fe775dc56a20eb00058ee8f9d8
Tyelr A Stank <tylerastank@gmail.com>
2024-05-23 07:54:35 -0500

    Print heading in Racebox demo script

--- read_racebox.py
+++ read_racebox.py
@@ -14,7 +14,7 @@ while True:
         ns = racebox_data["nanoseconds"] / 1e9
         seconds = racebox_data["second"] + ns
         if True:#time.time() > next_print:
-            print(f"{racebox_data['year']}-{racebox_data['month']:02}-{racebox_data['day']:02} {racebox_data['hour']:02}:{racebox_data['minute']:02}:{seconds:05.2f} | {racebox_data['latitude']:.1f}, {racebox_data['longitude']:.1f} | {racebox_data['speed_mph']:02.1f}mph | gx:{racebox_data['gforce_x']:1.2f} gy:{racebox_data['gforce_y']:1.2f} gz:{racebox_data['gforce_z']:1.2f}")
+            print(f"{racebox_data['year']}-{racebox_data['month']:02}-{racebox_data['day']:02} {racebox_data['hour']:02}:{racebox_data['minute']:02}:{seconds:05.2f} | {racebox_data['latitude']:.1f}, {racebox_data['longitude']:.1f} | {racebox_data['speed_mph']:02.1f}mph | gx:{racebox_data['gforce_x']:1.2f} gy:{racebox_data['gforce_y']:1.2f} gz:{racebox_data['gforce_z']:1.2f} | {racebox_data['heading']}")
             #next_print += print_interval_s
     except zmq.ZMQError:
         time.sleep(0.01)
